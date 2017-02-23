141ea81c66aa016b7ac7827f040d04b8cc05e40c
Thomas Li <txl306@student.bham.ac.uk>
2017-02-23 12:00:22 +0000

    Stringify res headers, again

--- routes/auth.js
+++ routes/auth.js
@@ -23,10 +23,10 @@ router.get('/', function(req, res, next) {
   var gh_req = https.request(options, (gh_res) => {
     console.log('Response: ' + gh_res)
     console.log('Status code: ' + gh_res.statusCode);
-    console.log('Response headers: ' + gh_res.headers);
+    console.log('Response headers: ' + JSON.stringify(gh_res.headers));
     gh_res.on('data', (d) => {
       ACCESS_TOKEN = d;
-      console.log('Payload: ', d.toString());
+      // console.log('Payload: ', d);
       // process.stdout.write(d);
     });
   });
