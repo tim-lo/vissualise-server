eb6195caad94f07c121a1fbe738b85a76847fadf
PernusBernus <tom9383@gmail.com>
2017-03-03 17:36:30 +0000

    Let's see if this helps

--- routes/auth.js
+++ routes/auth.js
@@ -11,37 +11,40 @@ var ACCESS_TOKEN;
 router.get('/', function(req, res, next) {
   console.log("Code value: " + req.query.code);
 
-  var options = {
-    method: 'POST',
-    hostname: 'github.com',
-    path: '/login/oauth/access_token?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + req.query.code,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }
+  getUserRepos();
 
-  var gh_req = https.request(options, (gh_res) => {
-    console.log('Response: ' + gh_res);
-    console.log('Status code: ' + gh_res.statusCode);
-    console.log('Response headers: ' + JSON.stringify(gh_res.headers));
-    gh_res.on('data', (d) => {
-      /* Return JSON format: {"access_token":"ACCESS_TOKEN","token_type":"bearer","scope":"repo,user:email"} */
-      ACCESS_TOKEN = JSON.parse(d.toString());
-      //console.log('Payload: ', d.toString());
-      getUserRepos();
-      res.render('graph', { title: 'That worked!', message: 'Code value: ' + req.query.code + ' Access token: ' + ACCESS_TOKEN["access_token"]});
-    });
-  });
+  // var options = {
+  //   method: 'POST',
+  //   hostname: 'github.com',
+  //   path: '/login/oauth/access_token?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + req.query.code,
+  //   headers: {
+  //     'Accept': 'application/json'
+  //   }
+  // }
 
-  gh_req.on('error', (e) => {
-    res.locals.message = e.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-    console.error(e);
-  });
+  // var gh_req = https.request(options, (gh_res) => {
+  //   console.log('Response: ' + gh_res);
+  //   console.log('Status code: ' + gh_res.statusCode);
+  //   console.log('Response headers: ' + JSON.stringify(gh_res.headers));
+  //   gh_res.on('data', (d) => {
+  //     /* Return JSON format: {"access_token":"ACCESS_TOKEN","token_type":"bearer","scope":"repo,user:email"} */
+  //     ACCESS_TOKEN = JSON.parse(d.toString());
+  //     //console.log('Payload: ', d.toString());
+  //     getUserRepos();
+  //     res.render('graph', { title: 'That worked!', message: 'Code value: ' + req.query.code + ' Access token: ' + ACCESS_TOKEN["access_token"]});
+  //   });
+  // });
 
-  gh_req.end();
+  // gh_req.on('error', (e) => {
+  //   res.locals.message = e.message;
+  //   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //   res.status(err.status || 500);
+  //   res.render('error');
+  //   console.error(e);
+  // });
+
+  // gh_req.end();
+  res.render('graph', { title: 'That worked!', message: 'cool beans'});
 });
 
 function getUserRepos() {
@@ -52,7 +55,8 @@ function getUserRepos() {
     path: '/user',
     headers: {
       "User-Agent": "Vissualise",
-      "Authorization": "token " + ACCESS_TOKEN["access_token"],
+      // "Authorization": "token " + ACCESS_TOKEN["access_token"],
+      "Authorization": "token 894bf70e7ce8e07eb5ed46b6db07202c367de68f",
       "Accept": "application/json"
     }
   }
