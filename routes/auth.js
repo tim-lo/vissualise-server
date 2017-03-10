129185b8ada5b4c5ec9244d01d9c17d89b86205d
PernusBernus <tom9383@gmail.com>
2017-03-10 22:59:13 +0000

    Let's see if this works

--- routes/auth.js
+++ routes/auth.js
@@ -45,9 +45,9 @@ router.get("/", function(req, res, next) {
     }
   };
   request.post(options, (error, response, body) => {
-    console.log('Error:', error);
-    console.log('Status Code:', response && response.statusCode);
-    console.log('Body:', body);
+    console.log("Error: ", error);
+    console.log("Status Code: ", response && response.statusCode);
+    console.log("Body: ", body);
     ACCESS_TOKEN = JSON.parse(body.toString());
     getUserRepos();
     res.render("graph", { title: "That worked!", message: "Code value: " + req.query.code + " Access token: " + ACCESS_TOKEN["access_token"]});
@@ -55,38 +55,6 @@ router.get("/", function(req, res, next) {
 });
 
 function getUserRepos() {
-  // var response_data;
-  // var options = {
-  //   method: "GET",
-  //   hostname: "api.github.com",
-  //   path: "/user",
-  //   headers: {
-  //     "User-Agent": "Vissualise",
-  //     "Authorization": "token " + ACCESS_TOKEN["access_token"],
-  //     "Accept": "application/json"
-  //   }
-  // };
-
-  // var gh_req = https.request(options, (gh_res) => {
-  //   console.log("Response: " + gh_res);
-  //   console.log("Status code: " + gh_res.statusCode);
-  //   console.log("Response headers: " + JSON.stringify(gh_res.headers));
-  //   gh_res.on("data", (d) => {
-  //     var t = d.toString();
-  //     // var u = JSON.parse(t);
-  //     console.log(t);
-  //   });
-  // });
-
-  // gh_req.on("error", (e) => {
-  //   res.locals.message = e.message;
-  //   res.locals.error = req.app.get("env") === "development" ? err : {};
-  //   res.status(err.status || 500);
-  //   res.render("error");
-  //   console.error(e);
-  // });
-  // gh_req.end();
-  
   var options = {
     url: "https://api.github.com/user",
     headers: {
@@ -96,9 +64,10 @@ function getUserRepos() {
     }
   };
   request.get(options, (error, response, body) => {
-    console.log('Error:', error);
-    console.log('Status Code:', response && response.statusCode);
-    console.log('Body:', body);
+    console.log("Error: ", error);
+    console.log("Status Code: ", response && response.statusCode);
+    console.log("Body: ", body);
+    console.log("Authenticated user: " + body.login);
   });
 }
 
